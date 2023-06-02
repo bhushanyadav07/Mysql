01f401f8ff14a95bf9bec156c7918814e07fffd4
Bhushan Yadav <30519282+bhushanyadav07@users.noreply.github.com>
2023-06-02 14:07:18 +0530

    Update leapcode.php

--- leapcode.php
+++ leapcode.php
@@ -1,38 +1,30 @@
-
 function getDaysInMonth($month, $year) {
 
   // Check for leap years
-  if ($year % 4 == 0) {
-    if ($year % 100 == 0 && $year % 400 != 0) {
-      // Not a leap year
+  if (is_leap_year($year)) {
+    if ($month == 2) {
+      $days = 29;
     } else {
-      // Leap year
+      $days = 31;
     }
   } else {
-    // Not a leap year
-  }
-
-  // Get the number of days in the month
-  switch ($month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      $days = 31;
-      break;
-    case 2:
-      if (is_leap_year($year)) {
-        $days = 29;
-      } else {
+    switch ($month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        $days = 31;
+        break;
+      case 2:
         $days = 28;
-      }
-      break;
-    default:
-      $days = 30;
-      break;
+        break;
+      default:
+        $days = 30;
+        break;
+    }
   }
 
   return $days;
@@ -44,7 +36,17 @@ function is_leap_year($year) {
 
   // Check if the year is divisible by 4
   if ($year % 4 == 0) {
-    return true;
+    // Check if the year is divisible by 100
+    if ($year % 100 == 0) {
+      // Check if the year is divisible by 400
+      if ($year % 400 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
   } else {
     return false;
   }
